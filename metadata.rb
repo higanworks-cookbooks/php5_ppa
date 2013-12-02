b748ac89e112221b2d7c3678685d40fe8a8c4378
sawanoboly <sawanoboriyu@higanworks.com>
2013-12-02 16:58:07 +0900

    bump 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "sawanoboriyu@higanworks.com"
 license          "Apache 2.0"
 description      "Installs/Configures php5 from ppa"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 supports         "ubuntu"
 
 depends           "apt"
